37e59d894f6b337176be88c6415a9860d2db8875
joaosmatos <joaovsmatoss@gmail.com>
2025-08-29 00:34:03 -0300

    analisador de numeros

---
+++ aula16ex/ex018/script.js
@@ -0,0 +1,28 @@
+let num = document.querySelector('input#fnum')
+let lista = document.querySelector('selecto#flista')
+let res = document.querySelector('div#res')
+let valores = []
+
+function isnumero(n) {
+    if(Number(n) >= 1 && Number(n) < 100) {
+        return true
+    } else {
+        return false
+    }
+}
+
+funtion inlista(n, l) {
+    if(l.indexOf(Number(n)) != -1) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function adicionar() {
+    if(isnumero(num.value) && !inlista(num.value, valores)) {
+
+    } else {
+        window.alert('Valor inválido ou já encontrado na lista.')
+    }
+}
