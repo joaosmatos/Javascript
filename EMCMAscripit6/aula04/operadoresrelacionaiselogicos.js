bc848a71d6ca6ade88a9cc5396f1370ab65cbcdf
joaosmatos <joaovsmatoss@gmail.com>
2025-09-19 00:39:24 -0300

    operadores relacionais e logicos

---
+++ EMCMAscripit6/aula04/operadoresrelacionaiselogicos.js
@@ -0,0 +1,28 @@
+// operadores relacionais e logicos
+var x = 4;
+var y = 2;
+
+console.log(x > y); // maior
+console.log(x < y); //menor
+console.log(x >= y); //maior ou igual
+console.log(x <- y); //menor ou igual
+console.log(x == y); //igual
+console.log(x != y); //diferente
+
+console.log(x === y); //identido
+console.log(x !== y); //não identico
+
+//OR, ou
+console.log(x > 3);
+console.log(y < 2);
+console.log(x > 3 || y < 2);
+
+//AND, e
+//OU
+console.log(x > 3);
+console.log(y < 2);
+console.log(x > 3 && y < 2);
+
+//not, não
+console.log(!true);
+console.log(!false)
