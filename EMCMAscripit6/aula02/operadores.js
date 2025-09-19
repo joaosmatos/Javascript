e38189ab418278ccc273590a7fe0b1ed51f3d9e8
joaosmatos <joaovsmatoss@gmail.com>
2025-09-19 00:18:08 -0300

    operadores

---
+++ EMCMAscripit6/aula02/operadores.js
@@ -0,0 +1,18 @@
+// operadores
+
+var x = 10;
+var y = 5;
+
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+console.log(x % y); //modulo é o resto da divisão
+console.log(x ** y); //exponenciação
+
+console.log(++x);
+console.log(x++);
+console.log(x);
+console.log(--y);
+console.log(y--);
+console.log(y);
