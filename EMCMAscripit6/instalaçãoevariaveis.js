10267910c76648b55c52106775d0daf30d6d840e
joaosmatos <joaovsmatoss@gmail.com>
2025-09-19 00:02:37 -0300

    variaveis

---
+++ EMCMAscripit6/instalaçãoevariaveis.js
@@ -0,0 +1,23 @@
+/*
+aula de variaies no ES6
+*/
+
+// variaveis ES6
+var x, y;
+x = 5;
+y = 6;
+var soma = x + y;
+console.log(soma)
+
+//variaveis ES6
+// let e const
+
+const PI = 3.14;
+console.log(PI);
+
+
+var array = [1, 2, 2, 4, 5, 6, 7, 8];
+for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    console.log(element)
+}
