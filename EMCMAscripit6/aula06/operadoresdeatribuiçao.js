b13d069791673ed823394e185b699175d628038c
joaosmatos <joaovsmatoss@gmail.com>
2025-09-20 00:29:56 -0300

    operadores de atribuição

---
+++ EMCMAscripit6/aula06/operadoresdeatribuiçao.js
@@ -0,0 +1,27 @@
+// operadores de atribuição
+var x = 100;
+var y = 5;
+
+console.log(x);
+x += y;
+console.log(x);
+x -= y;
+console.log(x);
+x *= y;
+console.log(x);
+x /= y;
+console.log(x);
+
+console.log(-x); // inverte o valor de x
+
+/*
+concatenar string
+var msg:
+var nome = "pedro"
+
+var msg = "ola" + nome + "!";
+
+var msg = `olá: ${nome}`
+
+console.log(msg)
+*/
