7452ec28b5c18d315b5b8035033fe6fc13e9dd01
joaosmatos <joaovsmatoss@gmail.com>
2025-09-28 01:42:43 -0300

    number parte 2

---
+++ EMCMAscripit6/aula17/number2.js
@@ -0,0 +1,17 @@
+// number partee 2
+
+var x = 0b010;
+var y = 0o011;
+var h = 0xf;
+
+console.log(x);
+
+//conversão de base
+var num = 123;
+console.log(
+   num.toString(10) 
+)
+
+var a = Number.NaN;
+
+console.log(a === a);
