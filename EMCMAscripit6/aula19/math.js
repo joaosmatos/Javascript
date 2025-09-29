2775ca77bf36f73e9855d70cff07b23aa117b66e
joaosmatos <joaovsmatoss@gmail.com>
2025-09-29 00:42:33 -0300

    math

---
+++ EMCMAscripit6/aula19/math.js
@@ -0,0 +1,63 @@
+//math
+
+console.log(
+    Math.E,
+    Math.LN2,
+    Math.LN10,
+    Math.LOG2E,
+    Math.LOG10E,
+    Math.SQRT1_2,
+    Math.SQRT2,
+    Math.PI
+)
+
+//esponenciação
+console.log(
+    4 ** 3,
+    Math.pow(4, 3)
+);
+
+//raiz
+console.log(
+    Math.sqrt(4),
+    Math.cbrt(8),
+    Math.pow(4, 2 / 4),
+    16 ** (1/4)
+);//sqrt é a raiz, cbrt é a raiz cubica
+
+//alogaritimo
+console.log(
+    Math.log10(100),
+    Math.log2(256),
+    Math.log(4) //logaritimo natural
+)
+
+//Função algébrica
+console.log(
+    Math.round(5.4), //Arredondamento
+    Math.cell(5.5), // ele sempre vai arredonda para cima
+    Math.floor(5.5) // ele sempre arredonda para baixo
+);
+
+//valor absoluto
+console.log(
+    Math.abs(5.04),
+    Math.abs(5.5)
+);
+
+console.log(
+    Math.sin(1,2,3,4,6,-2),//ele vai te retornar o minimo
+    Math.max(1,2,3,4,6,-2) //ele retorna o maximo
+);
+
+console.log(
+    Math.random(), //ele pega um numero aleatorio
+    Math.round(Math.random() * 10)
+);
+
+//trigonometria
+console.log(
+    Math.sin(),
+    Math.cos(1),
+    Math.tan(360)
+);
