dc9f19bf5f6c264d51ade411798b363b0b3454fa
joaosmatos <joaovsmatoss@gmail.com>
2025-10-13 00:45:53 -0300

    criei exercicio 34

--- EMCMAscripit6/aula34/promisesreforço.js
+++ EMCMAscripit6/aula34/promisesreforço.js
@@ -1,2 +1,19 @@
 // promises -> promessas
 
+var { fazAlgo1, fazAlgo2, fazAlgo3, erroCallback} = require("./promise-exemplo");
+
+console.log("sync")
+fazAlgo1()
+    .then(function (numero1) {
+        console.log("N1: " + numero1)
+        return fazAlgo2(numero1);
+    })
+    .then(function (numero2) {
+        console.log("N2: " + numero2)
+        return fazAlgo3(numero2);
+    })
+    .then(function (numero3) {
+        console.log("N3: " + numero3)
+    })
+    .catch(erroCallback);
+console.log("sync end")    
