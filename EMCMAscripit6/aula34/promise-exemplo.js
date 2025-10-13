5ef273dda62a2d13ee525b5eda31798241221c42
joaosmatos <joaovsmatoss@gmail.com>
2025-10-13 00:38:19 -0300

    add linhas de codigo

--- EMCMAscripit6/aula34/promise-exemplo.js
+++ EMCMAscripit6/aula34/promise-exemplo.js
@@ -31,3 +31,19 @@ var fazAlgo3 = function (numero) {
 function erroCallback(error) {
     console.log(`Deu ruim mostra a trajedia: ${error}`);
 }
+
+console.log("sync")
+fazAlgo1()
+    .then(function (numero1) {
+        console.log("N1: " + numero1)
+        return fazAlgo2(numero1);
+    })
+    .then(function (numero2) {
+        console.log("N2: " + numero2)
+        return fazAlgo3(numero2);
+    })
+    .then(function (numero3) {
+        console.log("N3: " + numero3)
+    })
+    .catch(erroCallback);
+console.log("sync end")    
