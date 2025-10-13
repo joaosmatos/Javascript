30c0f01c0f508fc4e7d4300bb5686bdf23e1e5ad
joaosmatos <joaovsmatoss@gmail.com>
2025-10-12 23:59:54 -0300

    criei simulação 2

---
+++ EMCMAscripit6/aula34/simulacao2.js
@@ -0,0 +1,21 @@
+// simulação de encadeamento -> callback rom hell
+
+fazAlgo1(function (primeiroResultado) {
+    fazAlgo2(primeiroResultado, function (segundoResultado) {
+        fazAlgo3(segundoResultado, function (terceiroResultado) {
+            console.log('ufa chegou no fim: ' + terceiroResultado);
+        }, erroCallback);
+    }, erroCallback);
+}, erroCallback);
+
+// melhor escrito no ES6
+fazAlgo1()
+    .then(function (primeiroResultado) {
+        return fazAlgo2(primeiroResultado);
+    })
+    .then(function (segundoResultado) {
+        return fazAlgo3(segundoResultado);
+    })
+    .then(function (terceiroResultado) {
+        console.log('ufa chegou no fim: ' + terceiroResultado);
+    })
